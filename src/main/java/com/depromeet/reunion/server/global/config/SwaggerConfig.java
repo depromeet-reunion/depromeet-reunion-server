026ea05c569ef0b7eac8616545695afef7824d47
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-07-30 14:39:19 +0900

    Feat : add Swagger to Bearer auth

--- src/main/java/com/depromeet/reunion/server/global/config/SwaggerConfig.java
+++ src/main/java/com/depromeet/reunion/server/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.depromeet.reunion.server.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +21,14 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI springOpenAPI() {
+
+        SecurityScheme bearerAuth = new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityItem = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", bearerAuth))
+                .addSecurityItem(securityItem)
                 .info(new Info().title("Depromeet Reunion Server API")
                         .description("Depromeet Reunion Server API 명세서입니다.")
                         .version("v0.0.1"));
